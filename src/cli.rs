af022e7da722e53c14eba35aa2e04fbf95837873
TLouison <toddmlouison@gmail.com>
2023-02-21 13:49:45 -0500

    Update board to always store (rank x file) squares

--- src/cli.rs
+++ src/cli.rs
@@ -26,11 +26,11 @@ fn prompt_location() -> Option<PieceLoc> {
 
 pub fn prompt_make_move(game: &Board) -> Option<Move> {
     if let Some(location) = prompt_location() {
-        if let Some(piece_result) = game.piece_exists_at_location(location) {
-            println!("Piece found: {:?}", piece_result.piece);
+        if let Some(piece) = game.get_piece_at_location(location) {
+            println!("Piece found: {:?}", piece);
             if let Some(target_location) = prompt_location() {
                 println!("Target chosen.");
-                return Some(Move::new(piece_result.index, location, target_location));
+                return Some(Move::new(piece, location, target_location));
             }
         } else {
             println!("No piece found at ({location:?})");
